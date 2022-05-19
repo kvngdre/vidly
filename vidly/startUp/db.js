389640987b25cc168227e690f118f2fb9610a0c0
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-05-19 23:24:41 +0100

    made changes to db.js

--- vidly/startUp/db.js
+++ vidly/startUp/db.js
@@ -1,9 +1,9 @@
 const logger = require("../middleware/logger");
 const mongoose = require('mongoose');
 
-
 module.exports = function() {
     mongoose.connect(process.env.DATABASE_URL)
             .then(() => console.log('Connected to database')) 
             .catch(err => console.log(err.message));
 }
+
