b50f97a2aa47f74e8c6768a50c3f25089f7a8375
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-05-20 00:07:32 +0100

    fix

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/vidly/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-require('./vidly/startUp/routes')(app);
 require('./vidly/startUp/db')();
+require('./vidly/startUp/routes')(app);
 
 
 // Listener
