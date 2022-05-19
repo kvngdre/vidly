d748ce0e8ded812a4f8a42bbd25210817b3917da
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-05-19 23:31:52 +0100

    added useNewUrlParser option to connect db

--- vidly/startUp/db.js
+++ vidly/startUp/db.js
@@ -2,7 +2,7 @@ const logger = require("../middleware/logger");
 const mongoose = require('mongoose');
 
 module.exports = function() {
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
             .then(() => console.log('Connected to database')) 
             .catch(err => console.log(err.message));
 }
